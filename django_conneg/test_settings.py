937c6b4130721cdba840072bc518f70b5d859458
Ben Carrillo <bennomadic@gmail.com>
2011-12-23 22:25:22 +0000

    fix typo in README; removed redundant test setting.

--- django_conneg/test_settings.py
+++ django_conneg/test_settings.py
@@ -1,8 +1,3 @@
-DATAASES = type('FakeDatabaseConfig',
-                 (dict,),
-                 {'__nonzero__': lambda self: True,
-                  '__contains__': lambda self, key: True})()
-
 INSTALLED_APPS = (
     'django_conneg',
 )
