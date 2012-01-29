6cd0049473895c8a86680ad8760ef733cd1603f8
Alexander Dutton <alexander.dutton@oucs.ox.ac.uk>
2012-01-29 21:58:01 +0000

    No longer requires tox for tests.

--- django_conneg/test_settings.py
+++ django_conneg/test_settings.py
@@ -1,6 +1,15 @@
+import imp
+
 INSTALLED_APPS = (
     'django_conneg',
-    'django_jenkins',
 )
+
+# Use django_jenkins if it's installed.
+try:
+    imp.find_module('django_jenkins')
+except ImportError:
+    pass
+else:
+    INSTALLED_APPS += ('django_jenkins',)
 
 DATABASES = {'default': {'ENGINE': 'django.db.backends.sqlite3'}}
